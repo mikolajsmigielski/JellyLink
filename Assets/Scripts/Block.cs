ba0279703bce669ea67cd7cd87234282b63dc4f9
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-09-02 14:57:14 +0200

    Improved Block.cs and Board.cs

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -18,17 +18,30 @@ public class Block : MonoBehaviour
 {
     [SerializeField]
     BlockType[] BlockTypes;
+
+    private Board Board;
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
     public BlockColor Color { get; private set; }
+    private Vector3 TargetPosition;
+
+    private void Awake()
+    {
+        Board = FindObjectOfType<Board>();
+    }
     void Start()
     {
+        
         Color = GetRandomColor();
         SetSprite();
     }
 
     
+
     void Update()
     {
-        
+        transform.localPosition = Vector3.Lerp(transform.localPosition, TargetPosition, Time.deltaTime * 5f);
     }
     public static BlockColor GetRandomColor()
     {
@@ -41,5 +54,11 @@ public class Block : MonoBehaviour
         var sprite = BlockTypes.First(type => type.Color == Color).Sprite;
         GetComponent<SpriteRenderer>().sprite = sprite;
 
+    }
+    public void Configure(int x, int y)
+    {
+        X = x;
+        Y = y;
+        TargetPosition = Board.GetBlockPosition(x, y);
     }
 }
