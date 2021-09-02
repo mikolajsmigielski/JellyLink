3088683bb55efa72054a94cce5be91bd3e131b68
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-09-02 12:53:54 +0200

    Added board generator

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField]
+    GameObject BlockPrefab;
+
+    [SerializeField]
+    [Range(3,8)]
+    int Height = 6;
+
+    [SerializeField]
+    [Range(3,10)]
+    int Width = 5;
+
+    [SerializeField]
+    [Range(0.5f, 5f)]
+    float GridSize = 2f;
+
+    [SerializeField]
+    [Range(0.5f, 2f)]
+    float BlockSize = 1f;
+
+    void Start()
+    {
+        GenerateBoard();
+    }
+
+    void Update()
+    {
+        
+    }
+    private void GenerateBoard()
+    {
+        for (int x = 0; x < Width; x++)
+            for (int y = 0; y < Height; y++)
+                GenerateBlock(x, y);
+    }
+
+    private void GenerateBlock(int x, int y)
+    {
+        var obj = Instantiate(BlockPrefab);
+        obj.transform.parent = transform;
+        obj.transform.localPosition = GetBlockPosition(x, y);
+        obj.transform.localScale = Vector3.one * BlockSize;
+    }
+    public Vector2 GetBlockPosition(int x, int y)
+    {
+        var basePosition = new Vector2(x - Width / 2f + 0.5f, y - Height / 2f - 0.5f);
+        return basePosition * GridSize;
+    }
+}
