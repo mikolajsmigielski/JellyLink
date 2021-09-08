8c76c59e77b902f18f892e131c6430a6be08c67b
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-09-08 15:58:29 +0200

    Added numbers script

---
+++ Assets/Scripts/Numbers.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum TextDirection { Left, Middle, Right}
+public class Numbers : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] Sprites;
+
+    [SerializeField]
+    float GridSize = 1f;
+
+    [SerializeField]
+    TextDirection TextDirection;
+
+    private int _value = 0;
+    public int Value
+    {
+        get { return _value; }
+        set
+        {
+            _value = value;
+            RefreshNumbers();
+        }
+    }
+    List<GameObject> DigitsObjects = new List<GameObject>();
+    void Start()
+    {
+        
+    }
+    
+    
+    void Update()
+    {
+        
+    }
+    private void RefreshNumbers()
+    {
+        RemoveDigits();
+
+        var digits = Value.ToString().Select(c => int.Parse(c.ToString())).ToArray();
+
+        for(int i = 0; i < digits.Count(); i++)
+        {
+            var position = CalculatePosition(i,digits.Count());
+            
+            var digit = CreateDigit(position, digits[i]);
+            DigitsObjects.Add(digit);
+        }
+    }
+
+    private void RemoveDigits()
+    {
+        DigitsObjects.ForEach(number => Destroy(number));
+        DigitsObjects.Clear();
+    }
+
+    private GameObject CreateDigit(Vector3 position, int value)
+    {
+        var digit = new GameObject();
+        digit.transform.parent = transform;
+        digit.transform.localPosition = position;
+
+        var sprite = Sprites[value];
+        digit.AddComponent<SpriteRenderer>().sprite = sprite;
+
+        return digit;
+    }
+
+    private Vector3 CalculatePosition(int i, int NumbersOfDigits )
+    {
+        var result = 0f;
+        if (TextDirection == TextDirection.Left)
+        {
+            result = i;
+        }
+        else if (TextDirection == TextDirection.Middle)
+        {
+            result = i - NumbersOfDigits / 2f + 0.5f; ;
+        }
+        else if (TextDirection == TextDirection.Right)
+        {
+            result = i - NumbersOfDigits + 1;
+        }
+        return Vector3.right * i * GridSize;
+    }
+}
