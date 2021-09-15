521f0462b5e7f537ad194aa0ec2e18c1669ced1e
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-09-15 18:27:23 +0200

    Added game manager and user interface

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    float GameDuration = 60f;
+
+    private float remainingTime;
+    public float RemainingTime
+    {
+        get { return remainingTime; }
+        private set
+        {
+            remainingTime = value;
+            if (remainingTime < 0f)
+                OnGameEnded();
+
+
+            if (OnRemainingTimeChange != null)
+                OnRemainingTimeChange.Invoke(remainingTime);
+        }
+    }
+
+    public event Action<float> OnRemainingTimeChange;
+
+    private int score;
+    public int Score
+    {
+        get { return score; }
+        private set
+        {
+            score = value;
+
+            if (OnScoreChange != null)
+                OnScoreChange.Invoke(score);
+        }
+    }
+
+
+    
+    public event Action<int> OnScoreChange;
+
+    void Start()
+    {
+        Score = 0;
+        RemainingTime = GameDuration;
+        StartCoroutine(TimeCounterCoroutine());
+
+        FindObjectOfType<BlocksConnection>().OnConection += UpdateScore;
+    }
+
+    
+
+    IEnumerator TimeCounterCoroutine()
+    {
+        while (true)
+        {
+            RemainingTime -= 1f;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+    private void OnGameEnded()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    private void UpdateScore(int lenght)
+    {
+        Score += lenght * lenght;
+    }
+}
---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour
+{
+    [SerializeField]
+    Numbers TimeCounter;
+
+    [SerializeField]
+    Numbers ScoreCounter;
+    void Start()
+    {
+        FindObjectOfType<GameManager>().OnRemainingTimeChange += time => TimeCounter.Value = (int)time;
+        FindObjectOfType<GameManager>().OnScoreChange += score => ScoreCounter.Value = score;
+    }
+
+    
+   
+}
